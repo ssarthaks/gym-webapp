ecfe962c4ecb270033491c7ebb17189468ac2c54
Sarthak Sharma <ssarthakxd@gmail.com>
2025-07-24 14:13:45 +0545

    Boiler plate for running server with TypeScript

---
+++ src/models/user.model.ts
@@ -0,0 +1,63 @@
+import { DataTypes, Model } from 'sequelize';
+import { sequelize } from '../config/database';
+
+export class User extends Model {
+  public id!: number;
+  public name!: string;
+  public email!: string;
+  public phone!: string;
+  public password!: string;
+  public previousPasswords!: string[];
+  public emailVerified!: boolean;
+  public isDeleted!: boolean;
+}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER.UNSIGNED,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        name: {
+            type: new DataTypes.STRING(128),
+            allowNull: false,
+        },
+        email: {
+            type: new DataTypes.STRING(128),
+            allowNull: false,
+            unique: true,
+        },
+        phone: {
+            type: new DataTypes.STRING(32),
+            allowNull: true,
+        },
+        address: {
+            type: new DataTypes.STRING(256),
+            allowNull: true,
+        },
+        password: {
+            type: new DataTypes.STRING(128),
+            allowNull: false,
+        },
+        previousPasswords: {
+            type: DataTypes.ARRAY(DataTypes.STRING),
+            allowNull: true,
+            defaultValue: [],
+        },
+        emailVerified: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false,
+        },
+        isDeleted: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false,
+        },
+    },
+    {
+        tableName: 'users',
+        sequelize,
+    }
+);
